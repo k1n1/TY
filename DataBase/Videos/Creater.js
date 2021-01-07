cbffdd2768e477cb02f5513d7ab6a1d7c90d123b
k1n1 <45115021+k1n1@users.noreply.github.com>
2021-01-07 22:39:36 +0530

    Update Creater.js

--- DataBase/Videos/Creater.js
+++ DataBase/Videos/Creater.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 const Creater = new mongoose.Schema({
+    user : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Auth"
+    },
     cid : {
         type:String,
         required:true
